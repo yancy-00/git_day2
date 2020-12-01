3e510ade3219c1fec81deb774e77b6e5486a7c2d
yancy <56312140@qq.com>
2020-12-01 21:24:49 +0800

    git_day2

---
+++ src/main/java/com/baizhi/git_day2/GitDay2Application.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_day2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDay2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDay2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/git_day2/GitDay2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.git_day2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDay2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
